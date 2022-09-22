c03a603e79f8784f2bb22d0a062578131e58b790
rekobernadus <rekobernadus@gmail.com>
2022-09-22 16:18:44 +0700

    project kedua

---
+++ application/controllers/latihan1.php
@@ -0,0 +1,20 @@
+<?php
+class latihan1  extends CI_controller
+{
+  public function index ()
+  {
+    echo "selamat datang.. selamat belajar web programming";
+    //$this->load->view('view-latihan1');
+  }
+
+  public function penjumlahan($n1, $n2)
+  {
+    $this->load->model('model_latihan1');
+    
+    $data['nilai1'] = $n1;
+    $data['nilai2'] = $n2;
+    $data['hasil'] = $this->model_latihan1->($n1, $n2);
+
+    $this->load->view('view_latihan1', $data)
+  }
+}
---
+++ application/views/view_latihan.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>latihan 1</title>
+    </head>
+    <body>
+       halo kawan.. yuk belajar web programming..!!!<br>
+       nilai 1 = <?= $nilai1; ?>
+       nilai 2 = <?= $nilai2; ?>
+       ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+        
+    </body>
+</html>
