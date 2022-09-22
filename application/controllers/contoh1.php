f872678ebb59cdeb74a3ec4949cb28ce87806795
rekobernadus <rekobernadus@gmail.com>
2022-09-22 16:19:12 +0700

    project kedua

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -3,8 +3,7 @@ class contoh1  extends CI_controller
 {
   public function index ()
   {
-    echo "nama saya reko bernadus";
+    echo "<h1>nama saya reko bernadus</h1>";
     echo "saya adalah mahasiswa Bina Sarana Informatika";
-
   }
 }
